c4bd6abb8fd2775c10b80c50d8a189eb35d2532a
Shubhdeep Singh <shubhdeep86993@gmail.com>
2022-02-04 13:36:13 +0000

    bug fixes, SVG added

--- src/components/sections/EmptySection.js
+++ src/components/sections/EmptySection.js
@@ -1,12 +1,27 @@
-import wave3 from '../../assets/wave3.png'
-import wave4 from '../../assets/wave4.png'
 
+export default function EmptySection(){
 
-export default function(){
 
     return<section className='empty_section'>
-                <img id='image_three' src={wave3} alt='img' />
-                <img id='image_four' src={wave4} alt='img' />
-                <img id='image_four_upside' src={wave4} alt='img' />
+                <svg id='svg_upside' viewBox={`0 0 1100 150`} preserveAspectRatio='none'>
+                  <path d="M388.007 115.455C186.205 120.538 0 89.314 0 89.314V0H1825V77.4539C1825 77.4539 1605.57 118.12 1439.92 124.894C1180.94 135.486 1061.22 66.3123 800.387 73.5812C627.481 78.3998 589.81 110.372 388.007 115.455Z" fill="#00071F"/>
+                </svg>
+                <svg id='svg_normal' viewBox={`0 0 1100 150`} preserveAspectRatio='none'>
+                    <g filter="url(#filter0_b_83_9)">
+                    <path d="M373.103 50.9882C223.718 49.124 0 24.1881 0 24.1881V137H1825V13.8804C1825 13.8804 1678.42 36.0828 1577.21 41.8585C1339.56 55.4193 1214.78 -1.6926 973.271 0.0386232C730.464 1.77916 615.779 54.0166 373.103 50.9882Z" fill="#858FB3"/>
+                    </g>
+                    <path d="M388.007 64.5453C186.205 59.4623 0 90.686 0 90.686V180H1825V102.546C1825 102.546 1605.57 61.8798 1439.92 55.1056C1180.94 44.5145 1061.22 113.688 800.387 106.419C627.481 101.6 589.81 69.6282 388.007 64.5453Z" fill="#00071F"/>
+                    <defs>
+                    <filter id="filter0_b_83_9" x="-10" y="-10" width="1845" height="157" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                    <feGaussianBlur in="BackgroundImage" stdDeviation="5"/>
+                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_83_9"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_83_9" result="shape"/>
+                    </filter>
+                    </defs>
+                </svg>
+
+
              </section>
 }
+
--- src/components/sections/SectionOne.js
+++ src/components/sections/SectionOne.js
@@ -1,22 +1,31 @@
 import '../../stylesheet/sectionOne.css'
 import Home from '../Home'
-import wave from '../../assets/wave1.png'
-import wave2 from '../../assets/wave2.png'
-import wave3 from '../../assets/wave3.png'
-import wave4 from '../../assets/wave4.png'
 import { useScrollSwitch } from '../../utils/scrollingSwitch'
 import { useScrolling } from '../../utils/scrolling'
 
 
-export default function (){
+export default function SectionOne(){
     const [scroll] = useScrolling()
     const [scrollSwitch] = useScrollSwitch(scroll, 8)
 
+
     return <section className='sec_1'> 
     {!scrollSwitch && <Home /> }
-    <img id='image_one' src={wave} alt='img' />
-    <img id='image_two' src={wave2} alt='img' />
-    <img id='image_three' src={wave3} alt='img' />
-    <img id='image_four' src={wave4} alt='img' />
+    <svg id='svg_four' viewBox={`0 0 1100 300`} fill="none" preserveAspectRatio='none'>
+    <path id='path_1' d="M326 197.49C129 197.49 0 0 0 0V335H1825V43.6957C1825 43.6957 1784 -8.40734 1479.5 104.366C1175 217.139 1115.5 14.8745 855 35.6106C594.5 56.3466 523 197.49 326 197.49Z"/>
+    <path id='path_2' d="M388.007 48.1958C186.205 35.5692 0 113.133 0 113.133V335H1825V142.595C1825 142.595 1605.57 41.5744 1439.92 24.7464C1180.94 -1.56327 1061.22 170.272 800.387 152.215C627.481 140.245 589.81 60.8224 388.007 48.1958Z"/>
+    <g filter="url(#filter0_b_80_2)">
+    <path id='path_3' d="M373.103 205.988C223.718 204.124 0 179.188 0 179.188V292H1825V168.88C1825 168.88 1678.42 191.083 1577.21 196.859C1339.56 210.419 1214.78 153.307 973.271 155.039C730.464 156.779 615.779 209.017 373.103 205.988Z" fill="#858FB3"/>
+    </g>
+    <path id='path_4' d="M388.007 219.545C186.205 214.462 0 245.686 0 245.686V335H1825V257.546C1825 257.546 1605.57 216.88 1439.92 210.106C1180.94 199.514 1061.22 268.688 800.387 261.419C627.481 256.6 589.81 224.628 388.007 219.545Z" fill="#00071F"/>
+    <defs>
+    <filter id="filter0_b_80_2" x="-10" y="145" width="1845" height="157" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+    <feGaussianBlur in="BackgroundImage" stdDeviation="5"/>
+    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_80_2"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_80_2" result="shape"/>
+    </filter>
+    </defs>
+    </svg>
   </section>
 }
